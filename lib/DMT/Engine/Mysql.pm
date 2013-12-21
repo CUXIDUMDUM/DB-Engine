bce0e340d2b55ff3106eb5baa615f2b649a4c812
Vijaykumar Jain <jainv1@lonlx20575.nomura.com>
2013-12-22 02:45:17 +0530

    documentation

--- lib/DMT/Engine/Mysql.pm
+++ lib/DMT/Engine/Mysql.pm
@@ -9,3 +9,28 @@ use Carp;
 extends 'MooseX::App::Cmd';
 
 1;
+
+=head1 NAME
+
+DMT::Engine::Mysql
+
+=head1 SYNOPSIS
+
+in some bin/mysql.pl
+
+use DMT::Engine::Mysql;
+
+my $o = DMT::Engine::Mysql->new();
+
+$o->run();
+
+and then
+
+$ perl bin/mysql.pl help
+
+$ perl bin/mysql.pl help command
+
+$ perl bin/mysql.pl command [<args>]
+
+
+=cut
